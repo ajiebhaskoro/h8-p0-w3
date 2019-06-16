1ce837e9868819396022fa4d7bb944e2801537be
Ajie <jie_bazz@yahoo.com>
2019-06-16 21:13:16 +0700

    init

---
+++ Exercise-5.js
@@ -0,0 +1,22 @@
+function palindrome(kata) {
+    balikKata = ""
+
+    for (var i = kata.length - 1; i >= 0; i--) {
+        balikKata = balikKata + kata[i];
+    }
+    if (kata === balikKata){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
+console.log(palindrome('enak')); // false
+console.log(palindrome('mamam')); // true
+console.log(palindrome('kakak')); // true
