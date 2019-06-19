e0cd8abe1120b70c512dee51c28425a826f3e948
Ajie <jie_bazz@yahoo.com>
2019-06-19 23:59:02 +0700

    modified exercise 5

--- Exercise-5.js
+++ Exercise-5.js
@@ -1,9 +1,10 @@
 function palindrome(kata) {
-    balikKata = ""
+    var balikKata = ''
 
-    for (var i = kata.length - 1; i >= 0; i--) {
+    for (let i = kata.length - 1; i >= 0; i--) {
         balikKata = balikKata + kata[i];
     }
+    
     if (kata === balikKata){
         return true;
     } else {
